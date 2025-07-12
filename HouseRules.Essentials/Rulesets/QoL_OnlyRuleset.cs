4364a5534ec2e28ba45413eb6e385b16b09f43ec
The Gray Alien <103865052+TheGrayAlien@users.noreply.github.com>
2025-07-12 00:21:33 -0400

    Update QoL ruleset
    Change wording.

--- HouseRules.Essentials/Rulesets/QoL_OnlyRuleset.cs
+++ HouseRules.Essentials/Rulesets/QoL_OnlyRuleset.cs
@@ -9,8 +9,8 @@
         {
             const string name = "QoL Only";
             const string displayname = "<b><color=#FF0000>Q<color=#FFFF00>o<color=#00FFFF>L <color=#FFFFFF>Only</b>";
-            const string description = "Quality of life rules for playing a better base game.";
-            const string longdesc = "Reviving a fallen hero removes stun and frozen effects.\nHero's abilites can't affect/hurt other heroes or pets.\nBarbarian can hook a lamp and throw it in the same turn.\n";
+            const string description = "Quality of life changes for playing a better base game.";
+            const string longdesc = "Reviving a fallen hero removes stun and frozen effects.\nHero's abilites can't affect/hurt other heroes or summons.\nBarbarian can hook a lamp and throw it in the same turn.\n";
 
             var reviveRemovesEffects = new ReviveRemovesEffectsRule(true);
             var grappleUhooked = new GrappleUnhookedRule(true);
