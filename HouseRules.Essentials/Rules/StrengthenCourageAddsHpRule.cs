dde2548c51598a55c33a701f12aa5636ec876b8d
The Gray Alien <103865052+TheGrayAlien@users.noreply.github.com>
2025-07-16 21:54:58 -0400

    Add renamed rule
    Add StrengthenCourageAddsHp rule.
    Will remove CourageShantyAddsHp when HR updates the name.

---
+++ HouseRules.Essentials/Rules/StrengthenCourageAddsHpRule.cs
@@ -0,0 +1,83 @@
+﻿namespace HouseRules.Essentials.Rules
+{
+    using Boardgame.BoardEntities;
+    using Boardgame.GameplayEffects;
+    using DataKeys;
+    using HarmonyLib;
+    using HouseRules.Core;
+    using HouseRules.Core.Types;
+    using UnityEngine;
+
+    public sealed class StrengthenCourageAddsHpRule : Rule, IConfigWritable<int>, IPatchable, IMultiplayerSafe
+    {
+        public override string Description => "Courage Shanty can also give a small heal";
+
+        private static int _globalAdjustments;
+        private static bool _isActivated;
+
+        private readonly int _adjustments;
+
+        public StrengthenCourageAddsHpRule(int adjustments)
+        {
+            _adjustments = adjustments;
+        }
+
+        public int GetConfigObject() => _adjustments;
+
+        protected override void OnActivate(Context context)
+        {
+            _globalAdjustments = _adjustments;
+            _isActivated = true;
+        }
+
+        protected override void OnDeactivate(Context context) => _isActivated = false;
+
+        private static void Patch(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StrengthenCourage), "UpdateEffectsOnTarget"),
+                postfix: new HarmonyMethod(
+                    typeof(StrengthenCourageAddsHpRule),
+                    nameof(StrengthenCourage_UpdateEffectsOnTarget_Postfix)));
+        }
+
+        private static void StrengthenCourage_UpdateEffectsOnTarget_Postfix(Target target)
+        {
+            if (!_isActivated)
+            {
+                return;
+            }
+
+            var piece = target.piece;
+            if (piece.GetStat(Stats.Type.InnateCounterDamageExtraDamage) == 69 || HR.SelectedRuleset.Name.Contains("Revolutions") || piece.GetStat(Stats.Type.InnateCounterDamageExtraDamage) == 55 || HR.SelectedRuleset.Name.Contains("Heroes "))
+            {
+                if (Random.Range(1, 101) > 66)
+                {
+                    if (piece.GetHealth() < piece.GetMaxHealth())
+                    {
+                        piece.DisableEffectState(EffectStateType.Heal);
+                        piece.EnableEffectState(EffectStateType.Heal);
+                        piece.effectSink.Heal(_globalAdjustments);
+                        piece.effectSink.RemoveStatusEffect(EffectStateType.Downed);
+                        piece.effectSink.RemoveStatusEffect(EffectStateType.Stunned);
+                        piece.effectSink.RemoveStatusEffect(EffectStateType.Frozen);
+                    }
+
+                    HR.ScheduleBoardSync();
+                }
+            }
+            else
+            {
+                if (piece.GetHealth() < piece.GetMaxHealth())
+                {
+                    piece.DisableEffectState(EffectStateType.Heal);
+                    piece.EnableEffectState(EffectStateType.Heal);
+                    piece.effectSink.Heal(_globalAdjustments);
+                    piece.effectSink.RemoveStatusEffect(EffectStateType.Downed);
+                }
+
+                HR.ScheduleBoardSync();
+            }
+        }
+    }
+}
